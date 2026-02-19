505d280f3630f172c36925bfb4ab2d0e00c5306f
Marcelo Henrique <marcellosh.12@gmail.com>
2026-02-18 23:27:58 -0300

    ATUALIZAÇÃO E CRIAÇÃO DO EXE

---
+++ CENTRAL/CENTRAL.ps1
@@ -0,0 +1,1 @@
+Start-Process "cmd.exe" -ArgumentList '/k "C:\Users\marcelohenrique-cdt\Documents\BAT\CENTRAL_BAT\CENTRAL.bat"'
---
+++ CENTRAL_BAT/CENTRAL.ps1
@@ -0,0 +1,1 @@
+Start-Process "cmd.exe" -ArgumentList '/k "C:\Users\Nanosistecck\Documents\GitHub\AutomacaoBat\CENTRAL_BAT\CENTRAL.bat"'
